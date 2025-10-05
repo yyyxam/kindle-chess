bc0a98acbab6a0411656e05b636de754d9b718d3
theos_mxy <wengmax@proton.me>
2025-10-05 02:19:54 +0200

    Draw chessboard with eips

--- kindle_KUAL/hellokindle/eips_test.sh
+++ kindle_KUAL/hellokindle/eips_test.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# SICHERHEITS-WRAPPER für Kindle Hello World Test
 
 EXTENSION_PATH="/mnt/us/extensions/hellokindle"
 LOG_FILE="/mnt/us/hellokindle/tmp/eips-test.log"
@@ -10,46 +9,68 @@ echo "Zeit: $(date)" >> "$LOG_FILE"
 
 cd "$EXTENSION_PATH"
 
-# Sicherheitscheck 3: Backup des aktuellen Displays (falls möglich)
 echo "Starte Test-App..." >> "$LOG_FILE"
 
 echo "First screen clear..." >> "$LOG_FILE"
 eips -c
-sleep 2
+eips ""
 
-echo "String-Print" >> "$LOG_FILE"
-eips "Teststring"
-sleep 2
-eips ''
-sleep 2
-eips -c
 
-echo "Highlighted String-Print" >> "$LOG_FILE"
-eips "Highlighted Teststring" -h
-sleep 2
-eips ''
-sleep 2
-eips -c
+# DRAW CHESSBOARD
+for j in 0 1 2 3; do
 
-echo "String-Print" >> "$LOG_FILE"
-eips 2 2 "Laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanger Teststring"
-sleep 2
-eips ''
-sleep 2
+    for i in 0 1 2 3; do
+        x=$((i * 268))
+        y=$((j * 268))
+        eips -d l=10,w=134,h=134 -x $x -y $y
+    done
 
-echo "Black-Horse-Transp-BG" >> "$LOG_FILE"
-eips -g "/mnt/us/hellokindle/assets/black_horse.png" # not working bc transparent background
-sleep 2
-eips ''
-sleep 2
+    for i in 0 1 2 3; do
+        x=$((134 + i * 268))
+        y=$(( 134 + j * 268))
+        eips -d l=10,w=134,h=134 -x $x -y $y
+        sleep 0.5
+    done
+
+done
+sleep 20
 eips -c
 
 
-echo "Black-Horse-white-BG" >> "$LOG_FILE"
-eips -g "/mnt/us/hellokindle/assets/black_horse_w_bg.png" # works, but see conversion command to 8-bit greyscale in wiki
-sleep 2
-eips ''
-sleep 2
+
+# echo "String-Print" >> "$LOG_FILE"
+# eips "Teststring"
+# sleep 2
+# eips ''
+# sleep 2
+# eips -c
+
+# echo "Highlighted String-Print" >> "$LOG_FILE"
+# eips "Highlighted Teststring" -h
+# sleep 2
+# eips ''
+# sleep 2
+# eips -c
+
+# echo "String-Print" >> "$LOG_FILE"
+# eips 2 2 "Laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanger Teststring"
+# sleep 2
+# eips ''
+# sleep 2
+
+
+# echo "Black-Horse-Transp-BG" >> "$LOG_FILE"
+# eips -g "/mnt/us/hellokindle/assets/black_horse.png" # not working bc transparent background
+# sleep 2
+# eips ''
+# sleep 2
+# eips -c
+
+# echo "Black-Horse-white-BG" >> "$LOG_FILE"
+# eips -g "/mnt/us/hellokindle/assets/black_horse_w_bg.png" # works, but see conversion command to 8-bit greyscale in wiki
+# sleep 2
+# eips ''
+# sleep 2
 
 # Sicherstellung: Display ist sauber
 echo "Clearing display" >> "$LOG_FILE"
