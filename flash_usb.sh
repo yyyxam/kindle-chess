2622ec65f1cf89c59f84e5519d6c7aef0571e90c
mxyyy <wengmax@proton.me>
2026-03-23 16:17:47 +0100

    zwischenstand

---
+++ flash_usb.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cd kindle_chess
+
+echo "Building Binaries"
+RUSTFLAGS="-C target-feature=+crt-static" cross build --target armv7-unknown-linux-musleabi --release
+
+echo "Copying Binaries to Kindle"
+yes | cp -r ./target/armv7-unknown-linux-musleabi/release/kindle-hello /run/media/mxy/Kindle/hellokindle/bin/
+
+cd ..
+
+echo "Deleting Logs before copying.."
+yes | rm ./kindle_KUAL/hellokindle/log/*
+
+echo "Temporarly moving token before copying.."
+mv ./kindle_KUAL/hellokindle/secrets/token.json ./
+
+echo "Flashing local KUAL-App to Kindle"
+cp -r ./kindle_KUAL/* /run/media/mxy/Kindle/
+
+echo "Moving token back.."
+mv ./token.json ./kindle_KUAL/hellokindle/secrets/
+echo "Done flashing!"
